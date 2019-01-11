73d34fc71a4eeac2702050da746a4a30aa16f847
Daniel Martins <martinsda@gmail.com>
2019-01-11 12:32:26 -0200

    Create UsefulStringStuff.java

---
+++ UsefulStringStuff.java
@@ -0,0 +1,21 @@
+import java.util.*; 
+import java.io.*;
+
+// str.split(",")
+// str.replaceFirst()
+// str.replace("x", "")
+// str.substring(0, 5)
+// str.concat()
+// str.charAt(i)
+// String.valueOf()
+// Integer.parseInt()
+
+// System.out.println()
+
+class Main {  
+
+  public static void main (String[] args) {  
+    System.out.print("Who's the best player in the world?"); 
+  }   
+  
+}
